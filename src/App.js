509ba6e6a73d18bf656df454be72ebe0859646d6
Azhar Sayyed <azhartsiddiqui@gmail.com>
2022-05-13 12:43:12 +0530

    Second step

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import "./App.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 function App() {
-  const counter = useSelector((state)=>state.counter  );
+  const counter = useSelector((state) => state.counter);
+
+  const dispatch = useDispatch();
+
+  const increment = () => {
+    dispatch({
+      type: "INC",
+    });
+  };
+  const decrement = () => {
+    dispatch({
+      type: "DEC",
+    });
+  };
+
+  const Addbyten = () => {
+    dispatch({ type: "ADDTEN", payload: 10 });
+  };
   return (
     <div>
       <h1>Counter App</h1>
       <h2>{counter}</h2>
+      <button onClick={increment}>Add</button>
+      <button onClick={decrement}>Remove</button>
+      <button onClick={Addbyten}>Add 10</button>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,24 @@
 import { createStore } from "redux";
 
 const reducerFn = (state = { counter: 0 }, action) => {
+    if(action.type==='INC'){
+        return{
+            counter:state.counter+1
+        }
+    }
+    
+    if(action.type==='DEC'){
+        return{
+            counter:state.counter-1
+        }
+    }
+
+    if(action.type==='ADDTEN'){
+        return{
+            counter:state.counter+action.payload
+        }
+    }
+
     return state;   
 };
 
